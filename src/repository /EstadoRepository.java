a4c8c0a9367f6f07daf8d6e5fd9f1bfaef99c760
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-28 18:42:12 -0500

    Update EstadoRepository.java

--- src/repository /EstadoRepository.java
+++ src/repository /EstadoRepository.java
@@ -1,1 +1,6 @@
+package com.plataforma_educativa.educativa.repository;
 
+import com.plataforma_educativa.educativa.model.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> { }

4ace72c80657705e1027101aba5bb96eb9d8ee02
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-28 18:42:44 -0500

    Create NivelRepository.java

---
+++ src/repository /NivelRepository.java
@@ -0,0 +1,6 @@
+package com.plataforma_educativa.educativa.repository;
+
+import com.plataforma_educativa.educativa.model.Nivel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NivelRepository extends JpaRepository<Nivel, Integer> { }

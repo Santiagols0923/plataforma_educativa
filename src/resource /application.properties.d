56881d3d59fd80db6f0275b2316b4d5484687f3d
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-28 18:43:43 -0500

    Update application.properties.d

--- src/resource /application.properties.d
+++ src/resource /application.properties.d
@@ -1,1 +1,12 @@
+# 1. Dirección y base de datos (Usando el puerto 1433 que habilitamos)
+spring.datasource.url=jdbc:sqlserver://localhost:1433;databaseName=plataforma_educativa;trustServerCertificate=true
 
+# 2. Credenciales que acabamos de configurar en SQL Management Studio
+spring.datasource.username=sa
+spring.datasource.password=DASTMUAL-2500
+spring.datasource.driver-class-name=com.microsoft.sqlserver.jdbc.SQLServerDriver
+
+# 3. Configuración de JPA (Para que Java gestione las tablas)
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.show-sql=true
+spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.SQLServerDialect

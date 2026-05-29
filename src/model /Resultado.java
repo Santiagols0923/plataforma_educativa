10c7c46e7e8adbfd8112ebed270b9adf65448e1d
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:32:49 -0500

    Update Resultado.java

--- src/model /Resultado.java
+++ src/model /Resultado.java
@@ -1,1 +1,36 @@
+package com.plataforma_educativa.educativa.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "RESULTADO", schema = "dbo")
+@Data
+public class Resultado {
+
+    @Id
+    @NotNull(message = "El id del resultado no puede ser null")
+    @Column(name = "id_resultado", nullable = false)
+    private Integer idResultado;
+
+    @NotNull(message = "El usuario no puede ser null")
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    @NotNull(message = "El ejercicio no puede ser null")
+    @ManyToOne
+    @JoinColumn(name = "id_ejercicio", nullable = false)
+    private Ejercicio ejercicio;
+
+    @NotNull(message = "La respuesta del usuario no puede ser null")
+    @Column(name = "respuesta_usuario", nullable = false)
+    private Integer respuestaUsuario;
+
+    @NotNull(message = "La fecha no puede ser null")
+    @Column(name = "fecha", nullable = false)
+    private LocalDateTime fecha;
+}

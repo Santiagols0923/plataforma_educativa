5b7c56616dd70b30846010b0d1938a0591a07a75
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:39:29 -0500

    Update UsuarioInsigniaRepository.java

--- src/repository /UsuarioInsigniaRepository.java
+++ src/repository /UsuarioInsigniaRepository.java
@@ -1,1 +1,9 @@
+package com.plataforma_educativa.educativa.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.plataforma_educativa.educativa.model.UsuarioInsignia;
+
+public interface UsuarioInsigniaRepository extends JpaRepository<UsuarioInsignia, Integer> {
+
+}

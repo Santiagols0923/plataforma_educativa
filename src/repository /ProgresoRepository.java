22295013f3e65516192c35a5b5f2d524f4672242
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:37:33 -0500

    Update ProgresoRepository.java

--- src/repository /ProgresoRepository.java
+++ src/repository /ProgresoRepository.java
@@ -1,1 +1,9 @@
+package com.plataforma_educativa.educativa.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.plataforma_educativa.educativa.model.Progreso;
+
+public interface ProgresoRepository extends JpaRepository<Progreso, Integer> {
+
+}

673c895793ced0f0b7546b32d897317a4cd9ebea
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:23:08 -0500

    Update NivelController.java

--- src/Controller /NivelController.java
+++ src/Controller /NivelController.java
@@ -1,19 +1,86 @@
 package com.plataforma_educativa.educativa.controller;
 
-import com.plataforma_educativa.educativa.model.Nivel;
-import com.plataforma_educativa.educativa.repository.NivelRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
+
+import jakarta.validation.Valid;
+
+import com.plataforma_educativa.educativa.model.Nivel;
+import com.plataforma_educativa.educativa.repository.NivelRepository;
 
 @RestController
 @RequestMapping("/niveles")
+@CrossOrigin("*")
 public class NivelController {
-    @Autowired
-    private NivelRepository nivelRepository;
 
+    // INYECCIÓN DEL REPOSITORY DE NIVELES
+    @Autowired
+    private NivelRepository repository;
+
+    /*
+     * CU-NIVEL | RF-12.25 | E12
+     * LISTAR TODOS LOS NIVELES
+     */
     @GetMapping
     public List<Nivel> listar() {
-        return nivelRepository.findAll();
+
+        /*
+         * CONSULTAR TODOS LOS NIVELES
+         * REGISTRADOS EN LA BASE DE DATOS
+         */
+        return repository.findAll();
+    }
+
+    /*
+     * CU-NIVEL | RF-12.26 | E12
+     * GUARDAR NUEVO NIVEL
+     */
+    @PostMapping
+    public Nivel guardar(
+
+            // VALIDAR DATOS RECIBIDOS
+            @Valid @RequestBody Nivel nivel) {
+
+        /*
+         * GUARDAR NIVEL
+         * EN BASE DE DATOS
+         */
+        return repository.save(nivel);
+    }
+
+    /*
+     * CU-NIVEL | RF-12.27 | E12
+     * BUSCAR NIVEL POR ID
+     */
+    @GetMapping("/{id}")
+    public Nivel buscar(
+
+            // RECIBIR ID DEL NIVEL
+            @PathVariable Integer id) {
+
+        /*
+         * CONSULTAR NIVEL
+         * POR SU ID
+         */
+        return repository.findById(id).orElse(null);
+    }
+
+    /*
+     * CU-NIVEL | RF-12.28 | E12
+     * ELIMINAR NIVEL
+     */
+    @DeleteMapping("/{id}")
+    public void eliminar(
+
+            // RECIBIR ID DEL NIVEL
+            @PathVariable Integer id) {
+
+        /*
+         * ELIMINAR NIVEL
+         * DE LA BASE DE DATOS
+         */
+        repository.deleteById(id);
     }
 }

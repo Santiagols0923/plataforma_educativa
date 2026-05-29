9c1b8e9e445003d45fad1979dac046f7ec5cc29d
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:39:47 -0500

    Update UsuarioRepository.java

--- src/repository /UsuarioRepository.java
+++ src/repository /UsuarioRepository.java
@@ -1,1 +1,15 @@
+package com.plataforma_educativa.educativa.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.plataforma_educativa.educativa.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    
+    Usuario findByCorreoAndContrasena(
+        String correo,
+        String contrasena);
+
+        
+       
+}

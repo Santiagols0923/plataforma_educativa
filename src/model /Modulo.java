a3e217ee82edaf527a9720a4b52aba90daa25941
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:31:31 -0500

    Update Modulo.java

--- src/model /Modulo.java
+++ src/model /Modulo.java
@@ -1,1 +1,26 @@
+package com.plataforma_educativa.educativa.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "MODULO", schema = "dbo")
+@Data
+public class Modulo {
+
+    @Id
+    @NotNull(message = "El id del módulo no puede ser null")
+    @Column(name = "id_modulo", nullable = false)
+    private Integer idModuloPk;
+
+    @NotBlank(message = "El nombre del módulo no puede estar vacío")
+    @Size(max = 100)
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @NotNull(message = "El curso no puede ser null")
+    @ManyToOne
+    @JoinColumn(name = "id_curso_fk", nullable = false)
+    private Curso curso;
+}

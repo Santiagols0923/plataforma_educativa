34a1d1a94a19d008471ac811bcf745f0dfc2353a
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:38:58 -0500

    Update RolRepository.java

--- src/repository /RolRepository.java
+++ src/repository /RolRepository.java
@@ -1,10 +1,9 @@
 package com.plataforma_educativa.educativa.repository;
 
-import com.plataforma_educativa.educativa.model.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import com.plataforma_educativa.educativa.model.Rol;
+
 public interface RolRepository extends JpaRepository<Rol, Integer> {
-    // No necesitas escribir nada más, Spring se encarga del resto.
+
 }

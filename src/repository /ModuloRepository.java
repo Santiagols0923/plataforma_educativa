5e0d4b674638798e8c09e70f7e14524610993d0d
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:36:41 -0500

    Update ModuloRepository.java

--- src/repository /ModuloRepository.java
+++ src/repository /ModuloRepository.java
@@ -1,1 +1,9 @@
+package com.plataforma_educativa.educativa.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.plataforma_educativa.educativa.model.Modulo;
+
+public interface ModuloRepository extends JpaRepository<Modulo, Integer> {
+
+}

c73517726a49e36a706da4a998b9680b4d8d6257
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:31:49 -0500

    Update Nivel.java

--- src/model /Nivel.java
+++ src/model /Nivel.java
@@ -1,20 +1,29 @@
 package com.plataforma_educativa.educativa.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
 
 @Entity
-@Table(name = "NIVEL")
+@Table(name = "NIVEL", schema = "dbo")
+@Data
 public class Nivel {
+
     @Id
-    @Column(name = "id_nivel")
+    @NotNull(message = "El id del nivel no puede ser null")
+    @Column(name = "id_nivel", nullable = false)
     private Integer idNivel;
 
-    @Column(name = "nombre", nullable = false, length = 50)
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(max = 25)
+    @Column(name = "nombre", nullable = false, length = 25)
     private String nombre;
 
-    // Getters y Setters
-    public Integer getIdNivel() { return idNivel; }
-    public void setIdNivel(Integer idNivel) { this.idNivel = idNivel; }
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    @Size(max = 50)
+    @Column(name = "descripcion", length = 50)
+    private String descripcion;
+
+    @NotNull(message = "El puntaje del nivel no puede ser null")
+    @Column(name = "puntaje_nivel", nullable = false)
+    private Integer puntajeNivel;
 }

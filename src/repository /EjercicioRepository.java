799422f260e407a66ad133b94ac85844a4222eda
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:35:56 -0500

    Update EjercicioRepository.java

--- src/repository /EjercicioRepository.java
+++ src/repository /EjercicioRepository.java
@@ -1,1 +1,9 @@
+package com.plataforma_educativa.educativa.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.plataforma_educativa.educativa.model.Ejercicio;
+
+public interface EjercicioRepository extends JpaRepository<Ejercicio, Integer> {
+
+}

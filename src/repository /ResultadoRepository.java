d88109ab332b33df56d18d6188d20ee0bfc8ee84
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:37:52 -0500

    Update ResultadoRepository.java

--- src/repository /ResultadoRepository.java
+++ src/repository /ResultadoRepository.java
@@ -1,1 +1,9 @@
+package com.plataforma_educativa.educativa.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.plataforma_educativa.educativa.model.Resultado;
+
+public interface ResultadoRepository extends JpaRepository<Resultado, Integer> {
+
+}

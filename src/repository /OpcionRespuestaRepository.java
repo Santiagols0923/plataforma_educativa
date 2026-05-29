1ad8d0cafa42ab6d0171e8760b8971d22a05145e
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:37:18 -0500

    Update OpcionRespuestaRepository.java

--- src/repository /OpcionRespuestaRepository.java
+++ src/repository /OpcionRespuestaRepository.java
@@ -1,1 +1,9 @@
+package com.plataforma_educativa.educativa.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.plataforma_educativa.educativa.model.OpcionRespuesta;
+
+public interface OpcionRespuestaRepository extends JpaRepository<OpcionRespuesta, Integer> {
+
+}

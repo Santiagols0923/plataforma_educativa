b528e4ca938d0ab93767c08c088b3493d77beaad
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:22:13 -0500

    Update ModuloVistaController.java

--- src/Controller /ModuloVistaController.java
+++ src/Controller /ModuloVistaController.java
@@ -1,1 +1,22 @@
+package com.plataforma_educativa.educativa.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ModuloVistaController {
+
+    /*
+     * CU-MODULO | RF-12.8 | E12
+     * MOSTRAR VISTA DE MÓDULOS EDUCATIVOS
+     */
+    @GetMapping("/modulo")
+    public String mostrarVistaModulo() {
+
+        /*
+         * RETORNAR VISTA HTML
+         * DEL PANEL DEL ESTUDIANTE
+         */
+        return "modulo";
+    }
+}

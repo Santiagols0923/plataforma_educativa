8199e853b5299b821c0ad648a5f8faf55659c678
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:31:13 -0500

    Update Insignia.java

--- src/model /Insignia.java
+++ src/model /Insignia.java
@@ -1,1 +1,21 @@
+package com.plataforma_educativa.educativa.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "INSIGNIA", schema = "dbo")
+@Data
+public class Insignia {
+
+    @Id
+    @NotNull(message = "El id de la insignia no puede ser null")
+    @Column(name = "id_insignia", nullable = false)
+    private Integer idInsignia;
+
+    @NotBlank(message = "El nombre de la insignia no puede estar vacío")
+    @Size(max = 10)
+    @Column(name = "nombre", nullable = false, length = 10)
+    private String nombre;
+}

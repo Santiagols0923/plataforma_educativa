2f6dc564c1761a21daf7381dd9dc1faba2b4a159
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:35:40 -0500

    Update CursoRepository.java

--- src/repository /CursoRepository.java
+++ src/repository /CursoRepository.java
@@ -1,1 +1,10 @@
+package com.plataforma_educativa.educativa.repository;
 
+import com.plataforma_educativa.educativa.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+    // Aquí puedes agregar métodos de búsqueda personalizados si los necesitas
+}

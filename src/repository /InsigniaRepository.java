bca283f290099a50fa9f22e7e42799eac016d32a
Santiago Londoño Serna <63606469+Santiagols0923@users.noreply.github.com>
2026-05-29 15:36:27 -0500

    Update InsigniaRepository.java

--- src/repository /InsigniaRepository.java
+++ src/repository /InsigniaRepository.java
@@ -1,1 +1,9 @@
+package com.plataforma_educativa.educativa.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.plataforma_educativa.educativa.model.Insignia;
+
+public interface InsigniaRepository extends JpaRepository<Insignia, Integer> {
+
+}
